580ca6acbdd995d2b6e802f8f326e01164d09bd1
lokesh matta <iamlokeshofficial@gmail.com>
2023-04-30 20:13:20 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+})
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        displayMessage('No Number!');
+    } else if (guess === secretNumber){
+
+        displayMessage('Correct Number!');
+        score++;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.number').textContent = secretNumber;
+
+        // CSS manipulation
+        document.querySelector('body').style.backgroundColor= '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+
+    } else if (guess !== secretNumber) {
+        if(score > 1){
+
+            displayMessage( guess > secretNumber ? 'Too High!' : 'Too Low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
 
